be7712f7a26c4cc27940e701d49c7177f051d00d
upisfree <upisfree@outlook.com>
2014-04-19 03:30:53 +0400

    Sort routes by number

--- MyWay/MainPage.xaml.cs
+++ MyWay/MainPage.xaml.cs
@@ -7,22 +7,59 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Globalization;
 using MyWay.Resources;
 
 namespace MyWay
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        public class GroupByNumber
+        {
+            public string Number { get; set; }
+            public string Type   { get; set; }
+            public string Desc   { get; set; }
+        }
+
+        public class KeyedList<TKey, TItem> : List<TItem>
+        {
+            public TKey Key { protected set; get; }
+
+            public KeyedList(TKey key, IEnumerable<TItem> items)
+                : base(items)
+            {
+                Key = key;
+            }
+
+            public KeyedList(IGrouping<TKey, TItem> grouping)
+                : base(grouping)
+            {
+                Key = grouping.Key;
+            }
+        }
+
         // Конструктор
         public MainPage()
         {
             InitializeComponent();
 
-            // Задайте для контекста данных элемента управления listbox пример данных
-            DataContext = App.ViewModel;
+            List<GroupByNumber> PlayList = new List<GroupByNumber>();
+            //Add your GroupByNumbers in PlayList
 
-            // Пример кода для локализации ApplicationBar
-            //BuildLocalizedApplicationBar();
+            PlayList.Add(new GroupByNumber() { Number = "1", Type = "трамвай", Desc = "Пос. Амурский-ПО \"Полет\"" });
+            PlayList.Add(new GroupByNumber() { Number = "10", Type = "автобус", Desc = "здесь что-то" });
+            PlayList.Add(new GroupByNumber() { Number = "101", Type = "автобус", Desc = "\"Полет\"" });
+            PlayList.Add(new GroupByNumber() { Number = "89", Type = "автобус", Desc = "Пос. Амурский-ПО" });
+            PlayList.Add(new GroupByNumber() { Number = "88", Type = "автобус", Desc = "Пос. Амурский-ПО" });
+            PlayList.Add(new GroupByNumber() { Number = "24", Type = "трамвай", Desc = "Пос. Амурский-ПО Пос. Амурский-ПО Пос. Амурский-ПО \"Полет\"" });
+            PlayList.Add(new GroupByNumber() { Number = "254", Type = "трамвай", Desc = "Пос. Амурский-ПО Пос. Амурский-ПО Пос. Амурский-ПО \"Полет\"" });
+
+            var groupedPlayList =
+                    from list in PlayList
+                    group list by list.Number[0] into listByGroup
+                    select new KeyedList<char, GroupByNumber>(listByGroup);
+
+            Routes.ItemsSource = new List<KeyedList<char, GroupByNumber>>(groupedPlayList);
         }
 
         // Загрузка данных для элементов ViewModel
@@ -38,21 +75,5 @@ namespace MyWay
         {
 
         }
-
-        // Пример кода для построения локализованной панели ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Установка в качестве ApplicationBar страницы нового экземпляра ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Создание новой кнопки и установка текстового значения равным локализованной строке из AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Создание нового пункта меню с локализованной строкой из AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
     }
 }

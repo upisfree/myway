2475a7614143f9c2ef9845169d3110129956962a
upisfree <upisfree@outlook.com>
2014-06-01 14:42:01 +0400

    Routes caching
    Former-commit-id: 4763a4258b40fe3ef8b029648d7d873cd71e9346

---
+++ MyWay/DataBase.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.IO.IsolatedStorage;
+using System.Windows;
+
+namespace MyWay
+{
+  class DataBase
+  {
+    // Запись в базу
+    public static void WriteToFile(string file, string text)
+    {
+      IsolatedStorageFile fileStorage = IsolatedStorageFile.GetUserStoreForApplication();
+      StreamWriter Writer = new StreamWriter(new IsolatedStorageFileStream(file, FileMode.Append, fileStorage));
+      Writer.WriteLine(text);
+      Writer.Close();
+    }
+
+    // Чтение из базы
+    public static string ReadFile(string file)
+    {
+      IsolatedStorageFile fileStorage = IsolatedStorageFile.GetUserStoreForApplication();
+      StreamReader Reader = null;
+
+      string result;
+      
+      try
+      {
+        Reader = new StreamReader(new IsolatedStorageFileStream(file, FileMode.Open, fileStorage));
+        string textFile = Reader.ReadToEnd();
+        result = textFile;
+        Reader.Close();
+
+        return result;
+      }
+      catch
+      {
+        return "false";
+      }
+    }
+
+    // Удаление базы
+    public static void DeleteFile(string file)
+    {
+      IsolatedStorageFile fileStorage = IsolatedStorageFile.GetUserStoreForApplication();
+      StreamWriter Writer = new StreamWriter(new IsolatedStorageFileStream(file, FileMode.Truncate, fileStorage));
+      Writer.Close();
+    }
+  }
+}
--- MyWay/MainPage.xaml.cs
+++ MyWay/MainPage.xaml.cs
@@ -10,88 +10,120 @@ using System.Windows.Navigation;
 
 namespace MyWay
 {
-    public partial class MainPage : PhoneApplicationPage
+  public partial class MainPage:PhoneApplicationPage
+  {
+    public class GroupByNumber
     {
-        public class GroupByNumber
-        {
-            public string Number { get; set; }
-            public string Type   { get; set; }
-            public string Desc   { get; set; }
-            public string ToStop { get; set; }
-        }
-
-        public class KeyedList<TKey, TItem> : List<TItem>
-        {
-            public TKey Key { protected set; get; }
-            public KeyedList(TKey key, IEnumerable<TItem> items) : base(items)    { Key = key; }
-            public KeyedList(IGrouping<TKey, TItem> grouping)    : base(grouping) { Key = grouping.Key; }
-        }
-
-        // Конструктор
-        public MainPage()
-        {
-          InitializeComponent();
-        }
-
-        // Загрузка данных для элементов ViewModel
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            if (!App.ViewModel.IsDataLoaded)
-            {
-                App.ViewModel.LoadData();
-
-                if (NetworkInterface.GetIsNetworkAvailable())
-                {
-                  ShowRoutesList();
-                }
-                else
-                {
-                  ShowError();
-                }
-            }
-        }
-
-        public async void ShowRoutesList()
-        {
-            List<GroupByNumber> RoutesList = new List<GroupByNumber>();
-
-            string htmlPage = "";
-
-            using (var client = new HttpClient())
-            {
-                htmlPage = await new HttpClient().GetStringAsync("http://t.bus55.ru/index.php/app/get_routes/");
-            }
-
-            HtmlDocument htmlDocument = new HtmlDocument();
-            htmlDocument.LoadHtml(htmlPage);
-
-            foreach (var a in htmlDocument.DocumentNode.SelectNodes("//a"))
-            {
-                var elem = a.ChildNodes.ToArray();
-
-                string number = elem[0].InnerText.Trim();
-                string type   = elem[1].InnerText.Trim();
-                string desc   = elem[2].InnerText.Trim();
-                string toStop = a.Attributes["href"].Value + "|" + number + " " + type;
-
-                RoutesList.Add(new GroupByNumber() { Number = number, Type = " " + type, Desc = desc, ToStop = toStop });
-            }
-
-            var groupedRoutesList =
-                    from list in RoutesList
-                    group list by list.Number[0] into listByGroup
-                    select new KeyedList<char, GroupByNumber>(listByGroup);
-
-            Util.RemoveLoader(Load);
-
-            Routes.ItemsSource = new List<KeyedList<char, GroupByNumber>>(groupedRoutesList);
-        }
-
-        public void ShowError()
-        {
-          Util.RemoveLoader(Load);
-          Error.Visibility = System.Windows.Visibility.Visible;
-          return;
-        }
+      public string Number { get; set; }
+      public string Type   { get; set; }
+      public string Desc   { get; set; }
+      public string ToStop { get; set; }
     }
+
+    public class KeyedList<TKey, TItem> : List<TItem>
+    {
+      public TKey Key { protected set; get; }
+      public KeyedList(TKey key, IEnumerable<TItem> items) : base(items)    { Key = key; }
+      public KeyedList(IGrouping<TKey, TItem> grouping)    : base(grouping) { Key = grouping.Key; }
+    }
+
+    // Конструктор
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+
+    // Загрузка данных для элементов ViewModel
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+      if (!App.ViewModel.IsDataLoaded)
+      {
+        App.ViewModel.LoadData();
+
+        if (DataBase.ReadFile("Routes.txt") == "false" || DataBase.ReadFile("Routes.txt") == "")
+          ShowRoutesList();
+        else
+          ShowRoutesListOffline();
+
+//        if (NetworkInterface.GetIsNetworkAvailable())
+      }
+    }
+
+    public async void ShowRoutesList()
+    {
+      List<GroupByNumber> RoutesList = new List<GroupByNumber>();
+
+      string htmlPage = "";
+
+      using (var client = new HttpClient())
+      {
+        htmlPage = await new HttpClient().GetStringAsync("http://t.bus55.ru/index.php/app/get_routes/");
+      }
+
+      HtmlDocument htmlDocument = new HtmlDocument();
+      htmlDocument.LoadHtml(htmlPage);
+
+      foreach (var a in htmlDocument.DocumentNode.SelectNodes("//a"))
+      {
+        var elem = a.ChildNodes.ToArray();
+
+        string number = elem[0].InnerText.Trim();
+        string type   = elem[1].InnerText.Trim();
+        string desc   = elem[2].InnerText.Trim();
+        string toStop = a.Attributes["href"].Value + "|" + number + " " + type;
+
+        DataBase.WriteToFile("Routes.txt", number + "|" + type + "|" + desc + "|" + a.Attributes["href"].Value);
+
+        RoutesList.Add(new GroupByNumber() { Number = number, Type = " " + type, Desc = desc, ToStop = toStop });
+      }
+
+      var groupedRoutesList =
+            from list in RoutesList
+            group list by list.Number[0] into listByGroup
+            select new KeyedList<char, GroupByNumber>(listByGroup);
+
+      Util.RemoveLoader(Load);
+
+      Routes.ItemsSource = new List<KeyedList<char, GroupByNumber>>(groupedRoutesList);
+    }
+
+    public void ShowRoutesListOffline()
+    {
+      List<GroupByNumber> RoutesList = new List<GroupByNumber>();
+
+      Array db = DataBase.ReadFile("Routes.txt").Split(new Char[] {'\n'});
+
+      foreach (string a in db)
+      {
+        try
+        {
+          Array line = a.Split(new Char[] {'|'});
+
+          string number = line.GetValue(0).ToString();
+          string type = line.GetValue(1).ToString();
+          string desc = line.GetValue(2).ToString();
+          string toStop = line.GetValue(3).ToString() + "|" + number + " " + type;
+
+          RoutesList.Add(new GroupByNumber() { Number = number, Type = " " + type, Desc = desc, ToStop = toStop });
+        }
+        catch { }
+      }
+
+      var groupedRoutesList =
+            from list in RoutesList
+            group list by list.Number[0] into listByGroup
+            select new KeyedList<char, GroupByNumber>(listByGroup);
+
+      Util.RemoveLoader(Load);
+
+      Routes.ItemsSource = new List<KeyedList<char, GroupByNumber>>(groupedRoutesList);
+    }
+
+    public void ShowError()
+    {
+      Util.RemoveLoader(Load);
+      Error.Visibility = System.Windows.Visibility.Visible;
+      return;
+    }
+  }
 }

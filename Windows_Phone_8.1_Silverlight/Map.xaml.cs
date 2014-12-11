8d595fa40c97ae7a4c2743cb6685d2710b14f7ee
upisfree <upisfree@outlook.com>
2014-12-11 06:18:23 +0600

    Fix app deploy
    Former-commit-id: 46d2415ba5841246ae702a77e5ccb1dc8b497422

--- Windows_Phone_8.1_Silverlight/Map.xaml.cs
+++ Windows_Phone_8.1_Silverlight/Map.xaml.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Device.Location;
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
@@ -15,10 +14,14 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using Windows.Devices.Geolocation;
 using Windows.System;
+using GART;
+using Location = System.Device.Location.GeoCoordinate;
+using GART.Data;
+using GART.Controls;
+
 
 namespace MyWay
 {
@@ -33,7 +36,7 @@ namespace MyWay
 
     protected async override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
     {
-      base.OnNavigatedTo(e);
+      ARDisplay.StartServices();
 
       string mode = "";
       string id = "";
@@ -54,6 +57,18 @@ namespace MyWay
           await ShowRoute(id);
         else if (mode == "stop" && NavigationContext.QueryString.TryGetValue("lon", out lon) && NavigationContext.QueryString.TryGetValue("lat", out lat))
           ShowStop(id, lat, lon);
+
+      AddNearbyLabels();
+
+      base.OnNavigatedTo(e);
+    }
+
+    protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+    {
+      // Stop AR services
+      ARDisplay.StopServices();
+
+      base.OnNavigatedFrom(e);
     }
 
     // Нажатие на клавишу «Назад»
@@ -616,6 +631,45 @@ namespace MyWay
           geocoordinate.Speed ?? Double.NaN,
           geocoordinate.Heading ?? Double.NaN
           );
+    }
+
+    /*****************************************
+     Всё, что связано с доп. реальностью
+    *****************************************/
+
+    private void AddLabel(Location location, string label)
+    {
+      // We'll use the specified text for the content and we'll let 
+      // the system automatically project the item into world space
+      // for us based on the Geo location.
+      ARItem item = new ARItem()
+      {
+        Content = label,
+        GeoLocation = location,
+      };
+
+      ARDisplay.ARItems.Add(item);
+    }
+
+    private void AddNearbyLabels()
+    {
+      // Start with the current location
+      var current = ARDisplay.Location;
+
+      // We'll add three Labels
+      for (int i = 0; i < 3; i++)
+      {
+        // Create a new location based on the users location plus
+        // a random offset.
+        Location offset = new Location()
+        {
+          Latitude = current.Latitude + ((double)new Random().Next(-60, 60)) / 100000,
+          Longitude = current.Longitude + ((double)new Random().Next(-60, 60)) / 100000,
+          Altitude = Double.NaN // NaN will keep it on the horizon
+        };
+
+        AddLabel(offset, "Location " + i);
+      }
     }
   }
 }

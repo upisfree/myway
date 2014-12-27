3e83a089f8d07af1c39ac776deb011112df1ca8c
upisfree <upisfree@outlook.com>
2014-12-28 04:50:23 +0600

    Predicts: Update on swype
    Former-commit-id: b2ac5ff4e1d8a8f0cbff381a5ee8949b3fb3e703

--- Windows_Phone_8.1_Silverlight/Predicts.xaml.cs
+++ Windows_Phone_8.1_Silverlight/Predicts.xaml.cs
@@ -1,11 +1,10 @@
 ﻿using HtmlAgilityPack;
 using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
 using System;
-using System.Diagnostics;
 using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 
@@ -16,10 +15,6 @@ namespace MyWay
     public StopPredict()
     {
       InitializeComponent();
-
-      Resources.Remove("PhoneAccentColor");
-      Resources.Add("PhoneAccentColor", Util.ConvertStringToColor("#FF455580"));
-      ((SolidColorBrush)Resources["PhoneAccentBrush"]).Color = Util.ConvertStringToColor("#FF455580");
     }
 
     private int progress = 0;
@@ -53,7 +48,7 @@ namespace MyWay
           UpdateProgress.Value = 0;
         }
 
-        UpdateProgress.Value = (float)progress / t; // формула для времени + настроки карты
+        UpdateProgress.Value = (float)progress / t;
 
         progress += i;
       });
@@ -68,13 +63,17 @@ namespace MyWay
       public string Time   { get; set; }
     }
 
-    public void ShowPredicts(string link)
+    public async void ShowPredicts(string link)
     {
       Util.Hide(NoPredicts);
       NoFuture.Opacity = 0;
       NoFuture_Flag = 0;
       Util.Hide(Error);
       Util.Show(Load);
+
+      Predicts.Items.Add(GetSwipeInfo());
+
+      await Task.Delay(100);
 
       if (Util.IsInternetAvailable())
       {
@@ -121,6 +120,9 @@ namespace MyWay
 
     private void _refresh()
     {
+      progress = 0;
+      UpdateProgress.Value = 0;
+      
       Predicts.Items.Clear();
 
       Util.Show(Load);
@@ -131,13 +133,11 @@ namespace MyWay
 
     private void Refresh(object sender, System.EventArgs e)
     {
-      progress = 0;
-      UpdateProgress.Value = 0;
-
       _refresh();
     }
 
-    // Анимации
+    #region Анимации
+
     private int NoFuture_Flag;
     private int NoFuture_RandomItem;
 
@@ -180,5 +180,106 @@ namespace MyWay
 
       Util.DoubleAnimation(NoFuture, new PropertyPath("Opacity"), da);
     }
+
+    #endregion
+
+    #region Обновление по свайпу
+
+    private Point _start;
+    private Point _current;
+    private Point _end;
+    private int border = 120;
+
+    private void Predicts_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+      _start = _current = new Point(e.GetPosition(LayoutRoot).X, e.GetPosition(LayoutRoot).Y);
+      _end = new Point(0, 0);
+    }
+
+    private void Predicts_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+      _current = new Point(e.GetPosition(LayoutRoot).X, e.GetPosition(LayoutRoot).Y);
+      int sub = (int)(_current.Y - _start.Y); // subtraction
+
+      if (sub > border)
+      {
+        SwipeInfoArrow_Animation(90, 270, 0.5);
+        ((TextBlock)((Grid)Predicts.Items[0]).Children[1]).Text = "отпусти, чтобы обновить";
+      }
+      else
+      {
+        SwipeInfoArrow_Animation(270, 90, 0.5);
+        ((TextBlock)((Grid)Predicts.Items[0]).Children[1]).Text = "потяни, чтобы обновить";
+      }
+   }
+
+    private void Predicts_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+      _end = new Point(e.GetPosition(LayoutRoot).X, e.GetPosition(LayoutRoot).Y);
+      int sub = (int)(_end.Y - _start.Y); // subtraction
+
+      if (sub > border)
+        _refresh();
+    }
+    
+    private Grid GetSwipeInfo()
+    {
+      Grid g = new Grid();
+      g.Margin = new Thickness(0, -100, 0, 0);
+      g.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+
+      TextBlock arrow = new TextBlock();
+      arrow.Text = "➔";
+      arrow.FontSize = 42;
+      arrow.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+      arrow.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+      RotateTransform rt = new RotateTransform();
+      rt.Angle = 90;
+      rt.CenterX = rt.CenterY = 25;
+      arrow.RenderTransform = rt;
+
+      TextBlock text = new TextBlock();
+      text.Text = "потяни, чтобы обновить";
+      text.FontSize = 24;
+      text.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+      text.Margin = new Thickness(0, 40, 0, 0);
+
+      g.Children.Add(arrow);
+      g.Children.Add(text);
+
+      return g;
+    }
+
+    private double oldFrom; // чтобы он одну и ту же анимацию постоянно не воспроизводил
+    private void SwipeInfoArrow_Animation(double from, double to, double time)
+    {
+      if (oldFrom == from)
+        return;
+      else
+        oldFrom = from;
+
+      Duration duration = new Duration(TimeSpan.FromSeconds(time));
+      Storyboard sb = new Storyboard();
+      sb.Duration = duration;
+
+      DoubleAnimation da = new DoubleAnimation();
+      da.Duration = duration;
+
+      sb.Children.Add(da);
+
+      RotateTransform rt = new RotateTransform();
+
+      Storyboard.SetTarget(da, rt);
+      Storyboard.SetTargetProperty(da, new PropertyPath("Angle"));
+      da.From = from;
+      da.To = to;
+
+      ((TextBlock)((Grid)Predicts.Items[0]).Children[0]).RenderTransform = rt;
+      ((TextBlock)((Grid)Predicts.Items[0]).Children[0]).RenderTransformOrigin = new Point(0.5, 0.5);
+
+      sb.Begin();
+    }
+
+    #endregion
   }
 }
